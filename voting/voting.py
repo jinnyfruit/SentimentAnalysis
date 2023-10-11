7e9017d0fe0d202975287fe9c787a6bafe75b95e
jinnyfruit <jinnyfruit0527@gmail.com>
2023-10-12 00:15:59 +0900

    vote system code update

--- voting/voting.py
+++ voting/voting.py
@@ -2,10 +2,10 @@ import pandas as pd
 from collections import Counter
 
 # 5개의 CSV 파일 이름
-csv_files = ['file1.csv', 'file2.csv', 'file3.csv', 'file4.csv', 'file5.csv']
+csv_files = ['test.csv', 'test.csv', 'test3.csv', 'test4.csv', 'test5.csv']
 
-# 데이터를 저장할 DataFrame 생성
-result_df = pd.DataFrame(columns=['id', 'sentiment'])
+# 동일한 ID에 대한 Hard Voting 결과를 저장할 딕셔너리 생성
+voting_results = {}
 
 # 각 CSV 파일을 읽어서 하드 보팅
 for file in csv_files:
@@ -19,8 +19,19 @@ for file in csv_files:
         sentiment_counts = Counter(sentiment_values)
         majority_sentiment = sentiment_counts.most_common(1)[0][0]
 
-        # 하드 보팅 결과를 결과 DataFrame에 추가
-        result_df = result_df.append({'id': unique_id, 'sentiment': majority_sentiment}, ignore_index=True)
+        # Hard Voting 결과를 딕셔너리에 저장
+        if unique_id in voting_results:
+            voting_results[unique_id].append(majority_sentiment)
+        else:
+            voting_results[unique_id] = [majority_sentiment]
+
+# 결과를 DataFrame으로 변환
+result_df = pd.DataFrame(columns=['id', 'sentiment'])
+for unique_id, sentiments in voting_results.items():
+    sentiment_counts = Counter(sentiments)
+    majority_sentiment = sentiment_counts.most_common(1)[0][0]
+    result_df = pd.concat([result_df, pd.DataFrame({'id': [unique_id], 'sentiment': [majority_sentiment]})],
+                          ignore_index=True)
 
 # 최종 결과를 새로운 CSV 파일로 저장
 result_df.to_csv('hard_voting_result.csv', index=False)
