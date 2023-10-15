327af0e8b779c2b73510abf48ca30e18953ef992
jinnyfruit <jinnyfruit0527@gmail.com>
2023-10-16 03:32:19 +0900

    code update

--- findOOV.py
+++ findOOV.py
@@ -2,10 +2,10 @@ import pandas as pd
 from transformers import AutoTokenizer
 
 # CSV 파일을 읽어옴 (파일 경로에 맞게 수정)
-df = pd.read_csv('train_fold_0.csv')
+df = pd.read_csv('splits/train_fold_0.csv')
 
-# 언어 모델 및 토크나이저 선택 (예: BERT)
-model_name = "bert-base-uncased"
+# 언어 모델 및 토크나이저 선택
+model_name = "facebook/muppet-roberta-large"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # 'text' 열에서 토크나이즈하고 OOV 단어를 찾음
