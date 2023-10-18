b983d58a3bb6f2631ea51eac77542e9614b11da8
jinnyfruit <jinnyfruit0527@gmail.com>
2023-10-18 16:19:49 +0900

    preprocessing code update

--- llma_Phrase.py
+++ llma_Phrase.py
@@ -1,11 +1,49 @@
+import pandas as pd
+from transformers import AutoTokenizer
+import transformers
+import torch
+import csv
 
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+# Load the training data
+df = pd.read_csv('splits/train_fold_0.csv')
 
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+# Filter data with sentiment values 0 and 1
+filtered_data = df[df['sentiment'].isin([0, 1])]
 
-input_text = "The teacher said it. Now I'm going to paraphrase the sentence, \"I'm feeling good today.\" Student, should we try it?"
-input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+# Initialize the Llama2 model and tokenizer
+model = "meta-llama/Llama-2-7b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
 
-outputs = model.generate(input_ids)
-print(tokenizer.decode(outputs[0]))
+# Create a list to store the augmented data
+augmented_data = []
+
+# Generate paraphrases and store in dataaugmentation.csv
+for index, row in filtered_data.iterrows():
+    text = row['text']
+    sequences = pipeline(
+        text,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=100,
+    )
+
+    for seq in sequences:
+        paraphrased_text = seq['generated_text']
+        print(paraphrased_text)
+        augmented_data.append([row['id'], text, row['sentiment'], paraphrased_text])
+
+# Save the augmented data to dataaugmentation.csv
+with open('dataaugmentation.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['id', 'text', 'sentiment', 'paraphrased_text'])
+    csvwriter.writerows(augmented_data)
+
+print("Data augmentation complete. Augmented data saved to dataaugmentation.csv.")
